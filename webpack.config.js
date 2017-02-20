89e309a0dfd453eedccb0a55c16fd4f8168cafa2
Kevin Jennison <kevin.jennison1@gmail.com>
2017-02-20 14:01:34 -0800

    Use style-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,41 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/js/app.js',
   output: {
     filename: 'js/app.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
       template: './src/app.html',
     }),
-    new ExtractTextPlugin('./css/app.css'),
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 9000
   },
   module: {
-    rules: [{
-      test: /\.css$/,
-      use: ExtractTextPlugin.extract({
-        use: 'css-loader'
-      })
-    }]
-  }
+    rules: [
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader?importLoaders=1"
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png)$/,
+        loader: 'file',
+        query: {
+          name: 'img/[name].[ext]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000',
+      }
+    ]
+  },
 };
