f2a79c2b9edb87ca752b8e9abd16cfec65b5c507
Kevin Jennison <kevin.jennison1@gmail.com>
2017-03-26 17:43:56 -0700

    Deploy script.

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+
+source ./.env
+
+if [ "$1" = "dev" ]; then
+  export STAGE=DEV
+  export S3_BUCKET=$DEV_S3_BUCKET
+fi
+if [ "$1" = "prod" ]; then
+  export STAGE=PRODUCTION
+  export S3_BUCKET=$PROD_S3_BUCKET
+fi
+if [ "$1" = "" ]; then
+  echo "Specify \"dev\" or \"prod\"."
+  echo "Exiting."
+  exit
+fi 
+
+echo "Going to deploy to $STAGE at $S3_BUCKET."
+echo "Is this OK? [y/n]"
+read response
+if [ $response != "y" ]; then
+  echo "Exiting."
+  exit
+fi
+
+echo "Deploying..."
+
+aws s3 cp dist/ s3://$S3_BUCKET --recursive
+
+echo "Done!"
