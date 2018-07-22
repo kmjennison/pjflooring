56972c84d782d8a347275c0e10d97666d0ba7c0b
Kevin Jennison <kevin.jennison1@gmail.com>
2018-07-22 16:26:37 -0700

    Replace portfolio modals with collage

--- src/js/agency.js
+++ src/js/agency.js
@@ -32,4 +32,32 @@
         }
     });
 
+    // BEGIN: collagePlus JQuery plugin
+
+    $(document).ready(function(){
+        var collagePlusElemSelector = '.portfolio-collage'
+        // Here we apply the actual CollagePlus plugin
+        function collage() {
+            $(collagePlusElemSelector).collagePlus(
+                {
+                    // change this to adjust the height of the rows
+                    'targetHeight' : 200,
+                    // change this to try different effects
+                    // valid effets = effect-1 to effect-6
+                    'effect' : "effect-1"
+                }
+            );
+        };
+        // This is just for the case that the browser window is resized
+        var resizeTimer = null;
+        $(window).bind('resize', function() {
+            // set a timer to re-apply the plugin
+            if (resizeTimer) clearTimeout(resizeTimer);
+            resizeTimer = setTimeout(collage, 200);
+        });
+
+        collage();
+    });
+    // END: collagePlus JQuery plugin
+
 })(jQuery); // End of use strict
